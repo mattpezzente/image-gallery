d2aa051c9ce628eae584b6b740983bf5bf2a4530
Matt <matt@Matts-MacBook-Pro-2.local>
2017-10-20 17:01:00 -0400

    fixed animation timings

--- js/main.js
+++ js/main.js
@@ -1,27 +1,35 @@
-let data;
-
 (()=> {
+	const globe = document.querySelector('.globe')
 
-	if (localStorage.getItem('pics') === null) {
+	if (sessionStorage.getItem('pics') === null) {
 		fetchUnSplash()
 	}
 
 	setTimeout(showImages, 2000)
 
+	globe.addEventListener('click', clickGlobe)
+
 })();
+
+function clickGlobe() {
+	this.style.animationPlayState = 'running'
+	setTimeout( () => {
+		this.style.animationPlayState = 'paused'
+	}, 8000)
+}
 
 function fetchUnSplash() {
 	const clientID = '41e3281f033234674af3526dd01467252ddb2f2d2b57bfb0f97c4a52a2af44f5'
 	fetch(`https://api.unsplash.com/photos/?client_id=${clientID}`)
 		.then(resp => resp.json())
 		.then(resp => {
-			fetchData = window.localStorage.setItem('pics', JSON.stringify(resp))
+			fetchData = sessionStorage.setItem('pics', JSON.stringify(resp))
 		})
 }
 
 function showImages() {
 	const gallery = document.querySelector('.gallery')
-	const localJSON = JSON.parse(localStorage.getItem('pics'))
+	const localJSON = JSON.parse(sessionStorage.getItem('pics'))
 	let i = 0
 	localJSON.forEach(image => {
 		i++
@@ -29,7 +37,7 @@ function showImages() {
 			let tempHTML = '<li class="li-show li-hover">'
 			tempHTML += `<img src="${image.urls['full']}">`
 			tempHTML += '<div class="img-desc">'
-			tempHTML += `<h3 class="img-title">Posted By: ${image.user['name']}</h3>`
+			tempHTML += `<h3 class="img-title">By: ${image.user['name']}</h3>`
 			tempHTML += `<p class="img-likes">Likes: ${image.likes}</p>`
 			tempHTML += '</div>'
 			tempHTML += '</li>'
